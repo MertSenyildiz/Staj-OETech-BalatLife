dcf13f178f223ed0f249c9fcd10279494b5085b5
MertSenyildiz <mert.senyildiz123@gmail.com>
2022-08-15 14:22:29 +0300

    Drawer'a eklentiler yapıldı

---
+++ lib/app/data/consts/drawer_items.dart
@@ -0,0 +1,27 @@
+const List<Map<String, String>> adminPanelItems = [
+  {'title': 'Cari Hesaplar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Cari Firmalar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Mülkler', 'whereTo': '/cariHesaplar'},
+  {'title': 'Hesaplamalar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Ödemeler', 'whereTo': '/cariHesaplar'},
+  {'title': 'Kısa Mesaj Sistemi', 'whereTo': '/cariHesaplar'},
+  {'title': 'Duyuru', 'whereTo': '/cariHesaplar'},
+  {'title': 'Satın Alımlar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Demirbaşlar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Aidatlar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Mizan', 'whereTo': '/cariHesaplar'},
+  {'title': 'Kullanıcılar', 'whereTo': '/cariHesaplar'},
+];
+
+const List<Map<String, String>> invoiceItems=[
+  {'title': 'Tablo', 'whereTo': '/cariHesaplar'},
+  {'title': 'Grafik', 'whereTo': '/cariHesaplar'},
+  {'title': 'Faturalar', 'whereTo': '/cariHesaplar'},
+];
+
+const List<Map<String, String>> announcementItems=[
+  {'title': 'Haberler', 'whereTo': '/cariHesaplar'},
+  {'title': 'Bildirimler', 'whereTo': '/cariHesaplar'},
+  {'title': 'Oylamalar', 'whereTo': '/cariHesaplar'},
+  {'title': 'Satın Alımlar', 'whereTo': '/cariHesaplar'},
+];
--- lib/app/ui/widgets/drawer_widget.dart
+++ lib/app/ui/widgets/drawer_widget.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
+import '../../data/consts/drawer_items.dart';
 import '../../data/repositories/auth_repository.dart';
 import '../../data/repositories/user_repository.dart';
 
-class DrawerWidget extends StatelessWidget{
-  String defaultImg="assets/images/profile.png";
+class DrawerWidget extends StatelessWidget {
+  String defaultImg = "assets/images/profile.png";
+  // TODO:Role Göre şekillendirme
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -14,36 +16,87 @@ class DrawerWidget extends StatelessWidget{
       child: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          DrawerHeader(child: Container(child: Row(
-            children: [
-              Container(width:140,height: 140,child: ClipRRect(borderRadius: BorderRadius.circular(100),child: FadeInImage(
-                fit: BoxFit.cover,
-                placeholder: AssetImage(defaultImg),
-                image:NetworkImage(UserRepository.instance.user!.resimYolu),
-                imageErrorBuilder:(context,error,stackTrace){return Image.asset(defaultImg);},
-              ),)),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
+          DrawerHeader(
+            child: Container(
+                child: Row(
+              children: [
+                Container(
+                    width: 140,
+                    height: 140,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: FadeInImage(
+                        fit: BoxFit.cover,
+                        placeholder: AssetImage(defaultImg),
+                        image: NetworkImage(
+                            UserRepository.instance.user!.resimYolu),
+                        imageErrorBuilder: (context, error, stackTrace) {
+                          return Image.asset(defaultImg);
+                        },
+                      ),
+                    )),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      UserRepository.instance.user!.isim +
+                          ' ' +
+                          UserRepository.instance.user!.soyIsim,
+                      style: TextStyle(color: Colors.blue, fontSize: 15),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(UserRepository.instance.user!.mail),
+                  ],
+                ),
+              ],
+            )),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            child: ListView(shrinkWrap: true, children: [
+              buildListTile(title:"profile", whereTo: '/profile',icon: Icons.person),
+              buildListTile(title:"home", whereTo: '/home',icon: Icons.home),
+                 // TODO:if(UserRepository.instance.user!.isim=="denem")...[Text("mrb")],
+              ExpansionTile(
+                title: Text('Admin Panel'),
+                leading: Icon(Icons.admin_panel_settings),
                 children: [
-                  Text(UserRepository.instance.user!.isim+' '+UserRepository.instance.user!.soyIsim,style: TextStyle(color: Colors.blue,fontSize: 15),),
-                  SizedBox(height: 5,),
-                  Text(UserRepository.instance.user!.mail),
+                  for (int i = 0; i < adminPanelItems.length; i++)
+                    buildListTile(title: adminPanelItems[i]['title']!, whereTo: adminPanelItems[i]['whereTo']!),
                 ],
               ),
-            ],
-          )),),
-          ListView(
-            shrinkWrap: true,
+              buildListTile(title:"message notifications", whereTo: '/home',icon: Icons.mail),
+              ExpansionTile(
+                title: Text('Faturalar'),
+                leading: Icon(Icons.bar_chart),
+                children: [
+                  for (int i = 0; i < invoiceItems.length; i++)
+                    buildListTile(title: invoiceItems[i]['title']!, whereTo: invoiceItems[i]['whereTo']!),
+                ],
+              ),
+              ExpansionTile(
+                title: Text('Duyurular'),
+                leading: Icon(Icons.announcement),
+                children: [
+                  for (int i = 0; i < announcementItems.length; i++)
+                    buildListTile(title: announcementItems[i]['title']!, whereTo: announcementItems[i]['whereTo']!),
+                ],
+              ),
+            ]),
           ),
-          Spacer(),
+          //Spacer(),
           Divider(),
           InkWell(
             child: ListTile(
               title: Text("settings".tr),
               enableFeedback: true,
               leading: Icon(Icons.settings),
-              onTap: (){Get.toNamed('/settings');},
+              onTap: () {
+                Get.toNamed('/settings');
+              },
             ),
           ),
           InkWell(
@@ -60,4 +113,15 @@ class DrawerWidget extends StatelessWidget{
     );
   }
 
+  buildListTile({required String title,IconData? icon,required String whereTo}){
+      return InkWell(
+        child: ListTile(
+          title: Text(title.tr),
+          leading: icon!=null? Icon(icon):null,
+          onTap: () {
+            Get.toNamed(whereTo);
+          },
+        ),
+      );
+  }
 }
